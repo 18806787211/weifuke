f6e73504973385c3213b6301484ab047c2bb33c4
18806787211 <shenweisheng126@126.com>
2016-09-13 17:35:27 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 //导航
 function navMove(obj){
-	var liw=obj.outerWidth();//获取li的宽度
-	var lileft=obj.position().left;//获取线条到左边的距离
+	var liw=obj.outerWidth();
+	var lileft=obj.position().left;
 	$(".nav-box-line").stop(false,true).animate({left:lileft+"px",width:liw+"px"},200);	
 }
 
@@ -22,7 +22,7 @@ $(".title-nav-six,.box-btn-two").click(function(){
 	
 });
 
-//获得焦点和失去焦点
+
 $(".box-zhanghao").focus(function(){
 	if($.trim($(".box-zhanghao").val())=="请输入账号")
 	{
@@ -53,7 +53,7 @@ $(".box-password").blur(function(){
 });
 
 //获得网页地址
-//alert(window.location.hash);获取地址代码
+
 function address(){
 	var addressObj=window.location.hash;
 	//alert(addressObj);
@@ -90,12 +90,12 @@ function address(){
 
 
 //angular
-var app=angular.module("myApp",["ngRoute"]);//创建一个应用对象，加载路由模块（路由的本质是链接跳转，路由就是做单页面应用的主要技术）
+var app=angular.module("myApp",["ngRoute"]);
 
 
 //应用的路由配置
 app.templatePath="template/";
-app.config(['$routeProvider',"$httpProvider",function($route){//$routeProvider是模块的功能，$route是对象
+app.config(['$routeProvider',"$httpProvider",function($route){
 	$route.when('/',{
 		templateUrl:app.templatePath+"index.html",
 		controller:'index'
@@ -124,24 +124,24 @@ app.controller('index',function($scope){
 	address();
 	
 	//幻灯片
-	var Flash={};//json，为了减少使用全局变量
+	var Flash={};
 	$(".flash-bottom a").click(function(){
-		
-		if($(this).hasClass("colorA"))//判断第一次触发的时候是否有这个标记（colorA），如果有直接跳出函数。
+	
+		if($(this).hasClass("colorA"))
 		{
-			return;//跳出函数
+			return;
 		}
-		clearTimeout(Flash.Do);//进来的时候不管三七二十一直接清除一下
-		var nowPos=$(this).index();//获取当前位置
-		var oldPos=$(".colorA").index(".flash-bottom a");//获得旧的位置
-		$(this).addClass("colorA").siblings(".colorA").removeClass();//给当前加colorA标，其它兄弟都去除该标记
-		$(".flash-middle li").eq(nowPos).stop(false,true).fadeIn(2000);//新的位置背景淡入
-		$(".flash-middle li").eq(oldPos).stop(false,true).fadeOut(2000);//旧的位置背景淡出
+		clearTimeout(Flash.Do);
+		var nowPos=$(this).index();
+		var oldPos=$(".colorA").index(".flash-bottom a");
+		$(this).addClass("colorA").siblings(".colorA").removeClass();
+		$(".flash-middle li").eq(nowPos).stop(false,true).fadeIn(2000);
+		$(".flash-middle li").eq(oldPos).stop(false,true).fadeOut(2000);
 			
-		eval("Flash.Out"+oldPos+"("+oldPos+","+nowPos+")");//eval尽量不用，有时候为了开发需求用一下是可以的，eval作用是字符串拼接并执行代码
-		Flash.Do=setTimeout(function(){//使用setTimeout方法有一个缺点，它会去全局找Flash.In这个变量。所以在这个代码中要加eval方法
+		eval("Flash.Out"+oldPos+"("+oldPos+","+nowPos+")");
+		Flash.Do=setTimeout(function(){
 			eval("Flash.In"+nowPos+"("+nowPos+","+oldPos+")");
-		},800);//setimeout（“字符串”，时间)幻灯片进入的时候给它个延迟时间并且执行代码的作用
+		},800);
 	});
 	
 	
@@ -173,12 +173,12 @@ app.controller('index',function($scope){
 	}
 	
 	
-	Flash.In1=function(oldPos,nowPos){//进入的时候幻灯片进入
-			if(oldPos<nowPos)//为了自动轮播能正常轮播，如果当前值大于旧的值直接给幻灯片向左偏移，为了进来的时候直接从左边进入
+	Flash.In1=function(oldPos,nowPos){
+			if(oldPos<nowPos)
 			{
 				$(".slidetwo1,.slidetwo2,.slidetwo3").css({"left":"-2000px"})
 			}
-			else//反之幻灯片向右偏移，为了进来的时候直接从右边进入
+			else
 			{
 				$(".slidetwo1,.slidetwo2,.slidetwo3").css({"left":"2000px"})
 			}
@@ -196,7 +196,7 @@ app.controller('index',function($scope){
 	}
 	Flash.Out1=function(oldPos,nowPos){//幻灯片2出去设置动画
 		
-		if(oldPos<nowPos)//中间的幻灯片出去的时候判断现在的值是否大旧的值，幻灯片向左移出
+		if(oldPos<nowPos)
 		{
 			$(".slidetwo3").stop(false,true).animate({"left":"580px"},500,function(){
 				$(this).stop(false,true).animate({"left":"-2000px"},300);
@@ -237,41 +237,41 @@ app.controller('index',function($scope){
 	}
 	
 	
-	Flash.Move=function(){//创建自动轮播函数
+	Flash.Move=function(){
 		var oldPos,nowPos,lastPos;
-		oldPos=$(".colorA").index(".flash-bottom a");//找到旧的位置
-		lastPos=$(".flash-bottom a").length-1;//找到幻灯片最后的位置
+		oldPos=$(".colorA").index(".flash-bottom a");
+		lastPos=$(".flash-bottom a").length-1;
 		nowPos=oldPos==lastPos?0:oldPos+1;
-		$(".flash-bottom a").eq(oldPos).removeClass("colorA");//给旧的位置去除color标记
-		$(".flash-bottom a").eq(nowPos).addClass("colorA");//给新的位置添加colorA标记
-		$(".flash-middle li").eq(nowPos).stop(false,true).fadeIn(2000);//新的位置背景淡入
-		$(".flash-middle li").eq(oldPos).stop(false,true).fadeOut(2000);//旧的位置背景淡出
-		eval("Flash.Out"+oldPos+"("+oldPos+","+nowPos+")");//eval尽量不用，有时候为了开发需求用一下是可以的，eval作用是字符串拼接并执行代码
-		Flash.Do=setTimeout(function(){//使用setTimeout方法有一个缺点，它会去全局找Flash.In这个变量。所以在这个代码中要加eval方法
+		$(".flash-bottom a").eq(oldPos).removeClass("colorA");
+		$(".flash-bottom a").eq(nowPos).addClass("colorA");
+		$(".flash-middle li").eq(nowPos).stop(false,true).fadeIn(2000);
+		$(".flash-middle li").eq(oldPos).stop(false,true).fadeOut(2000);
+		eval("Flash.Out"+oldPos+"("+oldPos+","+nowPos+")");
+		Flash.Do=setTimeout(function(){
 			eval("Flash.In"+nowPos+"("+nowPos+","+oldPos+")");
-		},800);//setimeout（“字符串”，时间)幻灯片进入的时候给它个延迟时间并且执行代码的作用
+		},800);
 	}
 	
-	Flash.Auto=window.setInterval(function(){//设置每隔6秒自动轮播
-		if(window.location.hash!="#/")//这个判断是为了在其他没有轮播幻灯片的效果时不要触发自动轮播的代码。
+	Flash.Auto=window.setInterval(function(){
+		if(window.location.hash!="#/")
 		{
 			
 			window.clearInterval(Flash.Auto);
-			Flash=null;//把Flash这个清空掉，减少内存空间
+			Flash=null;
 			return;
 		}
-		 Flash.Move();//调用轮播函数
+		 Flash.Move();
 	},6000);
-	$(".flashBT").mouseenter(function(){//鼠标移入到幻灯片下的3个按钮，暂停自动轮播
+	$(".flashBT").mouseenter(function(){
 		clearInterval(Flash.Auto);
 	});
-	$(".flashBT").mouseleave(function(){//鼠标移出继续轮播
+	$(".flashBT").mouseleave(function(){
 		Flash.Auto=window.setInterval(function(){
-			if(window.location.hash!="#/")//这个判断是为了在其他没有轮播幻灯片的效果时不要触发自动轮播的代码。
+			if(window.location.hash!="#/")
 			{
 				
 				window.clearInterval(Flash.Auto);
-				Flash=null;//把Flash这个清空掉，减少内存空间
+				Flash=null;
 				return;
 			}
 			Flash.Move();
@@ -349,7 +349,7 @@ app.controller('channel',function($scope){
 	
 	$("body").on("click",".js_select ul a",function(){
 		var parentObj=$(this).parents(".js_select");
-		var oldval=parentObj.children("input").val();//得到input里的旧的值
+		var oldval=parentObj.children("input").val();
 		var text=$(this).text();
 		
 		var val=$(this).data('val');
@@ -357,10 +357,10 @@ app.controller('channel',function($scope){
 		parentObj.children("input").val(val);
 		parentObj.children(".agent_text").text(text);
 		parentObj.children("ul").hide();
-		if(oldval!=val)//旧的值跟新的值是否一样，如果一样就不进入触发change事件
+		if(oldval!=val)
 		{
 			
-			parentObj.children("input").change();//值改变了，触发change
+			parentObj.children("input").change();
 		}
 		
 	})
@@ -368,25 +368,17 @@ app.controller('channel',function($scope){
 
 
 
-	//（2级联动）
-	//捕获错误 http://www.w3school.com.cn/js/js_errors.asp
-	/*try
-	{
-	   //在此运行代码
-	}
-	catch(err)
-	{
-	   //在此处理错误
+
 	}
 	*/
 	try{//捕获错误
 		function locationObj(json,Obj,clearHtml){
 			
 			Obj.html(clearHtml);//每次进入清空对象
-			var frag=document.createDocumentFragment();//创建碎片（js创建方法，所以用jquery的时候就要加$(frag)）
+			var frag=document.createDocumentFragment();
 			$.map(json,function(val,key){
-				var lis=$("<li><a href='javascript:void(0)'data-val="+key+">"+val+"</a></li>");//创建标签并设置属性跟属性值
-				$(frag).append(lis);//把lis插入到碎片中（即内存中）
+				var lis=$("<li><a href='javascript:void(0)'data-val="+key+">"+val+"</a></li>");
+				$(frag).append(lis);
 			});
 			
 			Obj.append(frag);
